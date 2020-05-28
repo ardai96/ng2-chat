5c8cfa4cbde1f339ecf0727e6065b9043ed3da95
mhyps <45096147+mhyps@users.noreply.github.com>
2020-05-28 17:37:04 +0200

    added token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,13 @@
-import { Environment } from './environment.model';
+import { Environment } from "./environment.model";
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
 export const environment: Environment = {
   production: false,
-  SOCKET_SERVER: 'https://chat-for-uam-students.herokuapp.com/',
-  jwt: 'tutaj-wklej-otrzymany-token'
+  SOCKET_SERVER: "https://chat-for-uam-students.herokuapp.com/",
+  jwt:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoibWF0aHlwQHN0LmFtdS5lZHUucGwiLCJhdmF0YXIiOiJodHRwczovL3JhbmRvbXVzZXIubWUvYXBpL3BvcnRyYWl0cy9tZW4vMzkuanBnIiwiaWQiOjIxLCJpYXQiOjE1OTA0MTM1ODQsImV4cCI6MTYyMTk0OTU4NH0.fB8LoRcgnHC1GGtATLJ-LhyyI_OEWpTW8n7D_AiaCNY",
 };
 
 /*
