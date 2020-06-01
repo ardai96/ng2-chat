1d7a4dc3b57af0100fc5cca236b5272e1f1e3463
kacper <neronim66@gmail.com>
2020-06-01 18:48:19 +0200

    feat(chat) fix Effect injection error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  StoreRouterConnectingModule,
+  RouterStateSerializer,
+} from '@ngrx/router-store';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { reducers } from './store/app.state';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
@@ -19,6 +24,8 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     AppRoutingModule,
     BrowserAnimationsModule,
     StoreModule.forRoot(reducers),
+    StoreRouterConnectingModule.forRoot(),
+    EffectsModule.forRoot([]),
     environment.production ? [] : StoreDevtoolsModule.instrument()
   ],
   providers: [
--- src/app/chat/store/chat.actions.ts
+++ src/app/chat/store/chat.actions.ts
@@ -2,5 +2,4 @@ import { createAction, props } from '@ngrx/store';
 
 export const actionSendChatMessage = createAction('[Chat] Send Message', props<ChatModule.SendMessageDTO>());
 export const actionAddMessage = createAction('[Chat] Add message', props<ChatModule.ChatMessage>());
-export const actionReceiveMessage = createAction('[Chat] Receive message', props<ChatModule.ChatMessage>());
 export const actionInitConnection = createAction('[Chat] Initialize connection');
